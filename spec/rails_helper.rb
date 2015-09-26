d101ecc4156219d668d6f7580c078212436f03aa
Sam Morgan <s_morgan@me.com>
2015-09-26 18:06:57 +0100

    add Warden helpers for RSpec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -50,4 +50,9 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
+  # Configure Warden helpers
+  config.include Warden::Test::Helpers
+  config.before :suite do
+    Warden.test_mode!
+  end
 end
